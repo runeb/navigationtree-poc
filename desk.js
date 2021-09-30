0327d5ec49bb0d4828bebb31a2f65a808cff04ba
Rune Botten <rbotten@gmail.com>
2021-09-30 13:32:58 +0200

    Commit last changes

--- desk.js
+++ desk.js
@@ -3,6 +3,21 @@ import React from "react";
 import TreeEdit from "./plugins/tree-edit/TreeEdit";
 import S from "@sanity/desk-tool/structure-builder";
 
+const Tree = (typeName) => {
+  return Object.assign(S.documentTypeList(typeName).serialize(), {
+    type: 'component',
+    component: TreeEdit,
+    options: {
+      titleField: "title",
+      parentRefField: "parent"
+    },
+    menuItems: S.documentTypeList(typeName)
+      .menuItems()
+      .serialize().menuItems,
+  });
+}
+
+
 export default () => {
   // Where we have our custom list structure
   return S.list()
@@ -11,12 +26,7 @@ export default () => {
       S.listItem()
         .title("Categories")
         .child(
-          S.component(TreeEdit)
-            .id("tree-edit")
-            .title("Edit hierarchy")
-            .child((id) => {
-              return S.editor()
-            })
+          Tree("category")
         ),
     ]);
 };
--- plugins/tree-edit/TreeEdit.js
+++ plugins/tree-edit/TreeEdit.js
@@ -2,30 +2,35 @@ import React, { useEffect, useState, useCallback } from "react";
 import IntentButton from "part:@sanity/components/buttons/intent";
 import SortableTree from "react-sortable-tree";
 import "react-sortable-tree/style.css?raw";
-import {usePaneRouter} from "@sanity/desk-tool"
+import { usePaneRouter } from "@sanity/desk-tool";
 import sanityClient from "part:@sanity/base/client";
 import { useEditState } from "@sanity/react-hooks";
 import { EditIcon } from "@sanity/icons";
 import config from "config:tree-edit";
 import { Card, Stack, Select, Button } from "@sanity/ui";
+import { DocumentPaneItemPreview } from "@sanity/desk-tool/lib/components/DocumentPaneItemPreview";
+import schema from "part:@sanity/base/schema"
 
 const client = sanityClient.withConfig({
   apiVersion: "2021-09-01",
 });
 
 const TreeEdit = (props) => {
+  const schemaType = schema.get(props.schemaTypeName)
   const [loading, setLoading] = useState(true);
   const [treeData, setTreeData] = useState([]);
-  const [type, setType] = useState(config.types[0]);
-
-  // Configurable stuff, should be passed in to here
-  const titleField = type.title;
+  const {navigateIntent} = usePaneRouter()
+  
+  const {titleField = "title", parentRefField = "parent"} = props.options
+  const queryParams = {
+    type: schemaType.name, titleField, parentRefField
+  }
+  const query = `* [_type == $type] {_type, _id, "parent": @[$parentRefField], "title": coalesce(@[$titleField], "Untitled")}`
 
   useEffect(() => {
-    const query = `* [_type == $type]`;
     const fetchData = async () => {
       setLoading(true);
-      client.fetch(query, { type: type.name }).then((categories) => {
+      client.fetch(query, queryParams).then((categories) => {
         const findChildren = (parentId) =>
           categories
             .filter((c) => c.parent && c.parent._ref === parentId)
@@ -55,35 +60,37 @@ const TreeEdit = (props) => {
     };
     fetchData();
     const subscription = client.observable
-      .listen(
-        query,
-        { type: type.name },
-        {
-          visibility: "query",
-        }
-      )
+      .listen(query, queryParams, {
+        visibility: "query",
+      })
       .subscribe(fetchData);
     return () => {
       subscription.unsubscribe();
     };
-  }, [type]);
+  }, [schemaType.name]);
 
   return (
     <div style={{ height: "100%" }}>
       {!loading && !treeData.length && <h2>No documents</h2>}
       <SortableTree
         generateNodeProps={({ node }) => {
+
           const hasChanges = node._id.match(/drafts./);
           const id = node._id.split("drafts.").pop();
+
           return {
-            buttons: [<ChildEditLink id={id}/>]
+            title: <DocumentPaneItemPreview
+              layout={"default"}
+              schemaType={schemaType}
+              value={node}
+            />,
+            onClick: () => navigateIntent("edit", {type: schemaType.name, id: node._id})
           };
         }}
         treeData={treeData}
         canDrop={() => !loading}
         onMoveNode={(params) => {
-          console.log("move", params);
-          console.log("index", params.path.pop());
+          console.log("moveNode", params)
           const { node, nextParentNode } = params;
           const patch = client.patch(node._id);
           if (!nextParentNode) {
@@ -101,24 +108,5 @@ const TreeEdit = (props) => {
     </div>
   );
 };
-
-const ChildEditLink = ({id}) => {
-  const {ChildLink} = usePaneRouter()
-  const Link = useCallback((linkProps) => <ChildLink {...linkProps} childId={id} />, [
-    ChildLink,
-    id,
-  ])
-
-  return (
-    <Button
-      as={Link}
-      text={
-        <>
-          Edit
-        </>
-      }
-    />
-  )
-}
 
 export default TreeEdit;
